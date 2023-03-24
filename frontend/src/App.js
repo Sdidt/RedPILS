4df23f9950b08804d24e9d83ffe8b85cae91f7e0
Abhishek Vaidyanathan <abhishekvaidyanathan@Abhisheks-MacBook-Pro-4.local>
2023-03-24 15:54:37 +0800

    updated UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
+import { BrowserRouter , Route,Routes} from 'react-router-dom';
+
 import logo from './logo.svg';
 import SearchPage from './search_page';
+import Navbar from './navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <SearchPage></SearchPage>
-      </header>
+    <div className="mainApp">
+      <Navbar/>
+      <div className='App'>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Routes>
+              <Route path='/' element={<SearchPage/>}/>
+              <Route path='/workexperience' element={<SearchPage />}/>
+              <Route path='/projects' element={<SearchPage />} />
+              <Route path='/about' element={<SearchPage/>} />
+            </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
--- frontend/src/search_page.js
+++ frontend/src/search_page.js
@@ -1,31 +1,133 @@
 import React, { useState } from 'react';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 
 const SearchPage = () => {
 
     const [query, setQuery] = useState('');
-    const [results, setResults] = useState([]);
+    const [results, setResults] = useState([0]);
     const handleSearch=(e)=>{
         e.preventDefault();
     }
+    const items = [
+        {
+          id: 0,
+          name: 'Cobol'
+        },
+        {
+          id: 1,
+          name: 'JavaScript'
+        },
+        {
+          id: 2,
+          name: 'Basic'
+        },
+        {
+          id: 3,
+          name: 'PHP'
+        },
+        {
+          id: 4,
+          name: 'Java'
+        }
+      ]
+    
+      const handleOnSearch = (string, results) => {
+        // onSearch will have as the first callback parameter
+        // the string searched and for the second the results.
+        console.log(string, results)
+      }
+    
+      const handleOnHover = (result) => {
+        // the item hovered
+        console.log(result)
+      }
+    
+      const handleOnSelect = (item) => {
+        // the item selected
+        console.log(item)
+      }
+    
+      const handleOnFocus = () => {
+        console.log('Focused')
+      }
+    
+      const formatResult = (item) => {
+        return (
+          <>
+            <span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span>
+            <span style={{ display: 'block', textAlign: 'left' }}>name: {item.name}</span>
+          </>
+        )
+      }
   return (
-    <div>
-      <h1>Suck it</h1>
-      <form onSubmit={handleSearch}>
-        <input type="text" value={query} onChange={e => setQuery(e.target.value)} />
-        <button type="submit">Search</button>
-      </form>
-      {results.length > 0 ? (
-        <ul>
-          {results.map(result => (
-            <li key={result.id}>
-              <a href={result.url}>{result.title}</a>
-              <p>{result.snippet}</p>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No results found.</p>
-      )}
+    <div className='SearchPageMain'>
+    <div className='SearchPageResults'>
+        <div className='SearchResults1'>
+        {/* <form onSubmit={handleSearch}>
+            <input type="text" value={query} onChange={e => setQuery(e.target.value)} />
+            <button type="submit">Search</button>
+        </form> */}
+        <ReactSearchAutocomplete
+                items={items}
+                onSearch={handleOnSearch}
+                onHover={handleOnHover}
+                onSelect={handleOnSelect}
+                onFocus={handleOnFocus}
+                autoFocus
+                formatResult={formatResult}
+            />
+            <br/>
+        {results.length > 0 ? (
+            // <ul>
+            //   {results.map(result => (
+            //     <li key={result.id}>
+            //       <a href={result.url}>{result.title}</a>
+            //       <p>{result.snippet}</p>
+            //     </li>
+            //   ))}
+            // </ul>
+            // <html>
+            // <body>
+            <div>
+            <div className='reddit_embed'>
+                <iframe
+                    id="reddit-embed"
+                    src="https://www.redditmedia.com//r/IndiaSpeaks/comments/zpre2v/2_judges_cant_decide_bjp_mps_strong_objection_on/j0uassz?limit=0/?ref\_source=embed\&amp;ref=share\&amp;embed=true&limit=5"
+                    sandbox="allow-scripts allow-same-origin allow-popups"
+                    style={{border: "none", overflow: "auto" }}
+                    height="500"
+                    width="1000"
+                ></iframe>
+            </div>
+            <div className='reddit_embed'>
+                <iframe
+                    id="reddit-embed"
+                    src="https://www.redditmedia.com//r/IndiaSpeaks/comments/zpre2v/2_judges_cant_decide_bjp_mps_strong_objection_on/j0uassz?limit=2/?ref\_source=embed\&amp;ref=share\&amp;embed=true&limit=5"
+                    sandbox="allow-scripts allow-same-origin allow-popups"
+                    style={{border: "none", overflow: "auto" }}
+                    height="500"
+                    width="1000"
+                ></iframe>
+            </div>
+            </div>
+            // </body>
+            // </html>
+            ) : (
+            <p>No results found.</p>
+        )}
+        </div>
+        <div className="side-box">
+            <div>
+                Top searched terms
+            </div>
+            <br/>
+            <div className='buttons-start'>
+            {items.map((object_ele,i)=>
+            <button className='btn-orange' onClick={onclick} key={i}>{object_ele.name}</button>
+            )}
+            </div>
+        </div>
+    </div>
     </div>
   );
 }
