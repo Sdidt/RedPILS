b7f9fc946fe5b8bfdc39339e3f8bfd94ecae46a8
Abhishek Vaidyanathan <abhishekvaidyanathan@Abhisheks-MacBook-Pro-4.local>
2023-03-29 17:45:32 +0800

    updated frontend

--- frontend/src/search_page.js
+++ frontend/src/search_page.js
@@ -3,13 +3,15 @@ import Iframe from 'react-iframe-click';
 import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 import DATA from './services/datalist'
 import dummy_data from "./services/topics.json"
+import ProfilePictureCopy from "./pic1.png";
 
 const SearchPage = () => {
 
   const [query, setQuery] = useState('');
   const [results, setResults] = useState(dummy_data);
   const [queryResults, setQueryResults] = useState([]);
-  const [dataCheck, setDataCheck] = useState(0);
+  const [dataCollect, setDataCollect] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const [divClicked, setDivClicked] = useState([])
   const handleSearch=(e)=>{
       e.preventDefault();
@@ -17,23 +19,23 @@ const SearchPage = () => {
   const items = [
       {
         id: 0,
-        name: 'Cobol'
+        name: 'BJP'
       },
       {
         id: 1,
-        name: 'JavaScript'
+        name: 'Congress'
       },
       {
         id: 2,
-        name: 'Basic'
+        name: 'AAP'
       },
       {
         id: 3,
-        name: 'PHP'
+        name: 'Kejriwal'
       },
       {
         id: 4,
-        name: 'Java'
+        name: 'Bla'
       }
     ]
   const dummy_results = [
@@ -47,31 +49,59 @@ const SearchPage = () => {
     }
   ]
   
-  const handleOnSearch = (string, results) => {
+  const handleOnSearch = async(string, results) => {
     // onSearch will have as the first callback parameter
     // the string searched and for the second the results.
-    console.log(string, results)
+    if(string!=''){
+      setSearchTerm(string)
+    }
   }
 
   const handleOnHover = (result) => {
     // the item hovered
-    console.log(result)
+    // console.log(result)
   }
 
   const handleOnSelect = (item) => {
     // the item selected
-    console.log(item)
+    // console.log(item)
   }
 
   const handleOnFocus = () => {
-    console.log('Focused')
+    // console.log('Focused')
+  }
+
+  const handleButtonSearch = async(searchTerm) => {
+    console.log(searchTerm)
+    if(searchTerm!=""){
+      const dummy_data_store = await DATA.QueryData(searchTerm)
+      console.log(dummy_data_store['topk'])
+      setDataCollect(dummy_data_store['topk'])
+    }
+    else{
+      const dummy_data_store = await DATA.QueryData('BJP')
+      console.log(dummy_data_store['topk'])
+      setDataCollect(dummy_data_store['topk'])
+    }
+  }
+
+  const handleKeywordSearch = async(searchTerm) => {
+    console.log(searchTerm)
+    const dummy_data_store = await DATA.QueryData(searchTerm)
+    console.log(dummy_data_store['topk'].length)
+    if(dummy_data_store['topk'].length==0){
+      const dummy_data_store = await DATA.QueryData('BJP')
+    }
+    console.log(dummy_data_store['topk'])
+    setDataCollect(dummy_data_store['topk'])
   }
 
   const formatResult = (item) => {
     return (
       <>
-        <span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span>
-        <span style={{ display: 'block', textAlign: 'left' }}>name: {item.name}</span>
+        {/* <span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span>
+        <span style={{ display: 'block', textAlign: 'left' }}>name: {item.name}</span> */}
+        <span style={{ display: 'block', textAlign: 'left' }}>{item.name}</span>
       </>
     )
   }
@@ -86,16 +116,16 @@ const SearchPage = () => {
   useEffect (() => {
     console.log("inside use effect")
     const getData = async () => {
-      const dummy_data_store = await DATA.QueryData()
-      console.log(dummy_data_store)
-      setQueryResults(dummy_data_store)
+      const dummy_data_store = await DATA.QueryData('BJP')
+      console.log(dummy_data_store['topk'])
+      setDataCollect(dummy_data_store['topk'])
       console.log(queryResults)
       setResults(dummy_results)
     }
     if(queryResults.length==0){
       getData();
     }
-  },[dataCheck])
+  },[queryResults])
   console.log(queryResults)
   console.log(results)
 
@@ -103,42 +133,60 @@ const SearchPage = () => {
     <div className='SearchPageMain'>
     <div className='SearchPageResults'>
         <div className='SearchResults1'>
-        <ReactSearchAutocomplete
+          <div className='SearchBarDiv'>
+            <div className='SearchBar'>
+            <ReactSearchAutocomplete
                 items={items}
                 onSearch={handleOnSearch}
                 onHover={handleOnHover}
                 onSelect={handleOnSelect}
                 onFocus={handleOnFocus}
                 autoFocus
+                showIcon={true}
+                placeholder="Search"
                 formatResult={formatResult}
+                styling={{borderRadius: "8px"}}
             />
-            <br/>
-        <div>
-          {console.log(results.length)}
-          {results.map((result,index) => 
-            <div className='reddit_embed' key={result.id}>
-                {/* <iframe
-                    id={result.id}
-                    src={result.embed_link}
+            </div>
+            <button className='SearchButton' onClick={()=>handleButtonSearch(searchTerm)}>Search</button>
+          </div>
+          <br/>
+          {console.log(dataCollect.length)}
+          {dataCollect.length > 0 ? (
+          <div>
+            {dataCollect.map((result,index) => 
+              <div className='reddit_embed' key={result.url}>
+                  {/* <iframe
+                      id={result.id}
+                      src={result.embed_link}
+                      sandbox="allow-scripts allow-same-origin allow-popups"
+                      style={{border: "none", overflow: "auto" }}
+                      height="500"
+                      width="1000"
+                  ></iframe> */}
+                  <Iframe
+                    // onInferredClick={() => console.log('You clicked')}
+                    onInferredClick={() => handleIframeClick(index)}
+                    id = {result.url}
+                    src={"https://www.redditmedia.com/"+(result.url).substring(23,((result.url).length-1))+"?limit=2/?ref\_source=embed\&amp;ref=share\&amp;embed=true&limit=5"}
                     sandbox="allow-scripts allow-same-origin allow-popups"
                     style={{border: "none", overflow: "auto" }}
-                    height="500"
-                    width="1000"
-                ></iframe> */}
-                <Iframe
-                  // onInferredClick={() => console.log('You clicked')}
-                  onInferredClick={() => handleIframeClick(index)}
-                  // id={`iframe-${index}-${result.id}`}
-                  id = {result.id}
-                  src={result.embed_link}
-                  sandbox="allow-scripts allow-same-origin allow-popups"
-                  style={{border: "none", overflow: "auto" }}
-                  height="500"
-                  width="1000"
-                ></Iframe>;
-            </div>
-            )}
-        </div>
+                    height = '300px'
+                    width="100%"
+                  ></Iframe>;
+              </div>
+              )}
+          </div>
+           ) : (
+          <div>
+          <div className='NoResultsDiv'>
+            Nothing to display here. Search for a term or click on commonly search terms to see more results.
+          </div> 
+          <div className='NoResultsImg'>
+            <img src = {ProfilePictureCopy} alt="Abhishek" className="about-img" style={{width:"40%",height:"40%"}}></img>
+          </div>
+          </div>
+          )}
         </div>
         <div className="side-box">
             <div>
@@ -147,7 +195,7 @@ const SearchPage = () => {
             <br/>
             <div className='buttons-start'>
             {items.map((object_ele,i)=>
-            <button className='btn-orange' onClick={onclick} key={i}>{object_ele.name}</button>
+            <button className='btn-orange' onClick={()=>handleKeywordSearch(object_ele.name)} key={i}>{object_ele.name}</button>
             )}
             </div>
         </div>
--- frontend/src/services/datalist.js
+++ frontend/src/services/datalist.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const QueryData = async () => {
+const DummyQueryData = async () => {
     const res = await axios.get("http://127.0.0.1:5000/dummy_query")
-    // const res = await axios.get("http://localhost:5000/dummy_query")
     if (res == null){
         console.log("oops")
         return;
@@ -13,6 +12,20 @@ const QueryData = async () => {
     const res_data = res.data
     return res_data;
 };
+
+const QueryData = async (searchTerm) => {
+    const whitespaceRemoved = searchTerm.replace(/\s/g, '+')
+    const res = await axios.get("http://127.0.0.1:5000/query?query="+whitespaceRemoved)
+    if (res == null){
+        console.log("oops")
+        return;
+    }
+    else{
+        console.log(res.data)
+    }
+    const res_data = res.data
+    return res_data;
+}
 
 const export_const = {
     QueryData
